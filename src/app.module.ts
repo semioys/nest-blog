7d2a1702f8ac7b4305d3a6e660320876f20d733c
semioys <semioys@gmail.com>
2020-06-29 17:03:37 +0300

    MongoDB was connected to application via Mongoose Module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot(
+      'mongodb+srv://nest-blog:yB4tNmbLWwkH41uz@mongocluster.qmu9a.mongodb.net/nest-blog?retryWrites=true&w=majority',
+      {
+        useNewUrlParser: true
+      }
+    ),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
