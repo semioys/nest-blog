a0da3a518db3e4c7a5ef99df9ac417c249d259bd
semioys <semioys@gmail.com>
2020-06-29 23:20:51 +0300

    Authentication middleware was added

--- src/blog/blog.module.ts
+++ src/blog/blog.module.ts
@@ -1,8 +1,9 @@
-import { Module } from '@nestjs/common';
+import { Module, NestModule, MiddlewareConsumer, RequestMethod } from '@nestjs/common';
 import { BlogService } from './blog.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { BlogPost, BlogPostSchema } from './schemas/blog-post.schema';
 import { BlogController } from './blog.controller';
+import { AuthenticationMiddleware } from 'src/common/authentication.middleware';
 
 @Module({
   imports: [
@@ -11,4 +12,12 @@ import { BlogController } from './blog.controller';
   providers: [BlogService],
   controllers: [BlogController],
 })
-export class BlogModule {}
+export class BlogModule implements NestModule {
+  configure(consumer: MiddlewareConsumer): MiddlewareConsumer | void {
+    consumer.apply(AuthenticationMiddleware).forRoutes(
+      { method: RequestMethod.POST, path: '/blog/post' },
+      { method: RequestMethod.PUT, path: '/blog/edit' },
+      { method: RequestMethod.DELETE, path: '/blog/delete' },
+    )
+  }
+}
